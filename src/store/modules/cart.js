39d31d5080373466f8b4a7b030ecd2e98c2004ff
Julius <juliusfaeustel@gmx.de>
2023-01-23 14:42:53 +0100

    fixed cart

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -2,101 +2,101 @@
 import shop from "@/api/shop";
 
 export default {
-  namespaced: true,
+    namespaced: true,
 
-  state: {
-    // {id, quantity}
-    items: [],
-    checkoutStatus: null
-  },
-
-  getters: {
-    cartProducts (state, getters, rootState, rootGetters) {
-      return state.items.map(cartItem => {
-        const product = rootState.products.items.find(product => product.id === cartItem.id)
-        return {
-          id: product.id,
-          title: product.title,
-          price: product.price,
-          quantity: cartItem.quantity
-        }
-      })
+    state: {
+        // {id, quantity}
+        items: [],
+        checkoutStatus: null
     },
 
-    cartTotal (state, getters) {
-      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
-    },
-  },
-
-  mutations: {
-    pushProductToCart (state, productId) {
-      state.items.push({
-        id: productId,
-        quantity: 1
-      })
-    },
-
-    popProductFromCart (state, productId) {
-      const i = state.items.indexOf(productId);
-      state.items.splice(i, 1);
-    },
-
-    incrementItemQuantity (state, cartItem) {
-      cartItem.quantity++
-    },
-
-    decrementItemQuantity (state, cartItem) {
-      cartItem.quantity--
-    },
-
-    setCheckoutStatus (state, status) {
-      state.checkoutStatus = status
-    },
-
-    emptyCart (state) {
-      state.items = []
-    }
-  },
-
-  actions: {
-    addProductToCart({state, getters, commit, rootState, rootGetters}, product) {
-      console.log(product);
-      console.log(product.id);
-      if (rootGetters['products/productIsInStock'](product)) {
-        const cartItem = state.items.find(item => item.id === product.id)
-        if (!cartItem) {
-          commit('pushProductToCart', product.id)
-        } else {
-          commit('incrementItemQuantity', cartItem)
-        }
-        commit('products/decrementProductInventory', product, {root: true})
-      }
-    },
-
-    removeProductFromCart({state, getters, commit, rootState, rootGetters}, product) {
-      console.log(product);
-      console.log(product.id);
-      const cartItem = state.items.find(item => item.id === product.id)
-      if (!cartItem) {
-        commit('popProductFromCart', product.id)
-      } else {
-        commit('decrementItemQuantity', cartItem)
-      }
-      commit('products/incrementProductInventory', product, {root: true})
-    },
-
-    checkout({state, commit}) {
-      // Stripe Implementation
-      shop.buyProducts(
-        state.items,
-        () => {
-          commit('emptyCart')
-          commit('setCheckoutStatus', 'success')
+    getters: {
+        cartProducts(state, getters, rootState, rootGetters) {
+            return state.items.map(cartItem => {
+                const product = rootState.products.items.find(product => product.id === cartItem.id)
+                return {
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    quantity: cartItem.quantity
+                }
+            })
         },
-        () => {
-          commit('setCheckoutStatus', 'fail')
-        }
-      )
+
+        cartTotal(state, getters) {
+            return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
+        },
     },
-  }
+
+    mutations: {
+        pushProductToCart(state, productId) {
+            state.items.push({
+                id: productId,
+                quantity: 1
+            })
+        },
+
+        popProductFromCart(state, productId) {
+            const i = state.items.indexOf(productId);
+            state.items.splice(i, 1);
+        },
+
+        incrementItemQuantity(state, cartItem) {
+            cartItem.quantity++
+        },
+
+        decrementItemQuantity(state, cartItem) {
+            cartItem.quantity--
+        },
+
+        setCheckoutStatus(state, status) {
+            state.checkoutStatus = status
+        },
+
+        emptyCart(state) {
+            state.items = []
+        }
+    },
+
+    actions: {
+        addProductToCart({state, getters, commit, rootState, rootGetters}, product) {
+            console.log(product);
+            console.log(product.id);
+            if (rootGetters['products/productIsInStock'](product)) {
+                const cartItem = state.items.find(item => item.id === product.id)
+                if (!cartItem) {
+                    commit('pushProductToCart', product.id)
+                } else {
+                    commit('incrementItemQuantity', cartItem)
+                }
+                commit('products/decrementProductInventory', product, {root: true})
+            }
+        },
+
+        removeProductFromCart({state, getters, commit, rootState, rootGetters}, product) {
+            console.log(product);
+            console.log(product.id);
+            const cartItem = state.items.find(item => item.id === product.id)
+            if (cartItem.quantity == 1) {
+                commit('popProductFromCart', product.id)
+            } else {
+                commit('decrementItemQuantity', cartItem)
+            }
+            commit('products/incrementProductInventory', product, {root: true})
+        },
+
+        checkout({state, commit}) {
+            // Stripe Implementation
+            shop.buyProducts(
+                state.items,
+                () => {
+                    commit('emptyCart')
+                    commit('setCheckoutStatus', 'success')
+                },
+                () => {
+                    commit('setCheckoutStatus', 'fail')
+                }
+            )
+        },
+    }
 }
